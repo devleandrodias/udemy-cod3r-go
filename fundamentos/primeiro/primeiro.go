ddd2e313387f5fa4acaa122648b42ea0da31b405
Leandro <leandrodbdias@gmail.com>
2020-04-29 13:14:52 -0300

    Primeiro programa com GO

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Primeiro programa com...\n")
+	fmt.Print("Go Lang")
+}
+
+// Comentário de uma linha
+
+/*
+  Comantários de múltiplas linhas
+  muitas, muitas,
+  muitas linhas
+*/
+
+/*
+  1. Priorize código legível e faça comentários que agregue valor
+  2. Evite comentários óbvios
+  3. Durante o curso abuse dos comentários
+*/
