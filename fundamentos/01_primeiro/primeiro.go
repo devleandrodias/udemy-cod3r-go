0c7b5340ed325d902a9f9f944d2118d46ee40b6d
devleandrodias <leandrodbdias@gmail.com>
2020-08-17 22:42:48 -0300

    revisando primeiro programa em go

---
+++ fundamentos/01_primeiro/primeiro.go
@@ -0,0 +1,29 @@
+// Programas exucutáveis iniciam pelo pacote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar um ou
+vários imports
+*/
+import "fmt" // IO Formats
+
+// A porta de entrada de um programa Go é a função main
+func main() {
+	fmt.Print("Primeiro programa com...\n")
+	fmt.Print("Go Lang!\n")
+}
+
+// Comentário de uma linha
+
+/*
+  Comantários de múltiplas linhas
+  muitas, muitas,
+  muitas linhas
+*/
+
+/*
+  1. Priorize código legível e faça comentários que agregue valor
+  2. Evite comentários óbvios
+  3. Durante o curso abuse dos comentários
+*/
