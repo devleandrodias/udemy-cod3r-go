92c35481b54fd2a0c89665562331994e0a846715
devleandrodias <leandrodbdias@gmail.com>
2020-08-25 19:13:34 -0300

    revisando prints em go

--- fundamentos/04_prints/prints.go
+++ fundamentos/04_prints/prints.go
@@ -11,26 +11,26 @@ func main() {
 	fmt.Println("esse virá na próxima linha.")
 
 	x := 3.141516
-	xs := fmt.Sprint(x)
+	xs := fmt.Sprint(x) // Imprimir valor inteiro em formato de string
 
-	fmt.Println("O valor de x é: ", x)
+	fmt.Println("O valor de x é:", x)
 	fmt.Print("O valor de x é: " + xs)
-	fmt.Printf("O valor de x é: %.2f", x) // Imprimir valor formatado
+	fmt.Printf("O valor de x é: %.3f", x) // Imprimir valor formatado
 
 	a := 1
 	b := 1.9999
 	c := false
 	d := "opa"
 
-	/*
-		\n pular linha
-		%d tipo inteiro
-		%f tipo decimal
-		%t tipo boleano
-		%s tipo string
-		%v tipo genérico
-		%.1f tipo decimal formatado
-	*/
+	// /*
+	// 	\n pular linha
+	// 	%d tipo inteiro
+	// 	%f tipo decimal
+	// 	%t tipo boleano
+	// 	%s tipo string
+	// 	%v tipo genérico
+	// 	%.1f tipo decimal formatado
+	// */
 
 	fmt.Printf("\n%d %f %.1f %t %s", a, b, b, c, d)
 	fmt.Printf("\n%v %v %v %v %v", a, b, b, c, d)
